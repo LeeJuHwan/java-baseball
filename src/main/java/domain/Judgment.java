38017b4fac53455925b07c9abc58090163d58aab
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-08-23 00:41:40 +0900

    feat: 플레이어의 행동을 점수로 기록 할 심판 도메인 구성

---
+++ src/main/java/domain/Judgment.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Judgment {
+
+    private final Score score = new Score();
+    private final ArrayList<String> computerRandomNumbers;
+
+
+    public Judgment(Computer computer) {
+        this.computerRandomNumbers = computer.generateRandomNumber();
+        System.out.println("computerRandomNumbers = " + computerRandomNumbers);
+    }
+
+    public Score judge(ArrayList<String> userInputNumbers) {
+        score.clean();
+
+        for (int seq = 0; seq < userInputNumbers.size(); seq++) {
+            scoreRecord(userInputNumbers, seq);
+        }
+        score.printScoreRecordResult();
+        return score;
+    }
+
+    private void scoreRecord(ArrayList<String> userInputNumbers, int i) {
+        if (isStrike(userInputNumbers, i)) {
+            score.strikeIncrement();
+            return;
+        }
+
+        if (isBall(userInputNumbers, i)) {
+            score.ballIncrement();
+        }
+    }
+
+    private boolean isBall(ArrayList<String> userInputNumbers, int i) {
+        return computerRandomNumbers.contains(userInputNumbers.get(i));
+    }
+
+    private boolean isStrike(ArrayList<String> userInputNumbers, int i) {
+        return computerRandomNumbers.get(i).equals(userInputNumbers.get(i));
+    }
+}
