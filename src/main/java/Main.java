133bcdfd3c330aeaf392996b91b3b7c69c13d76f
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-08-24 01:59:20 +0900

    refactor: 메인 어플리케이션 레벨에서 게임 실행 도우미 메서드 제거

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,15 @@
 import app.BaseballApplication;
 import base.Application;
-import core.AppException;
 import io.OutputHandler;
 
 public class Main {
 
     public static void main(String[] args) {
         Application baseballApplication = new BaseballApplication();
-
         try {
-            startGame(baseballApplication);
+            baseballApplication.startGame();
         } catch (Exception e) {
             OutputHandler.printDefaultExceptionMessage(e);
-        }
-    }
-
-    private static void startGame(Application gameApplication) {
-        boolean isGameRunning = true;
-
-        while (isGameRunning) {
-            gameApplication.run();
-
-            isGameRunning = getGameStatusOption(gameApplication);
-        }
-    }
-
-    private static boolean getGameStatusOption(Application gameApplication) {
-
-        try {
-            String gameFlag = gameApplication.selectGameRestartOrStop();
-
-            if (gameApplication.isGameRestart(gameFlag)) {
-                return true;
-            }
-
-            if (gameApplication.isGameStop(gameFlag)) {
-                return false;
-            }
-
-            throw new AppException("게임 시작 명령어를 잘못 입력 하였습니다.");
-
-        } catch (AppException e) {
-            OutputHandler.printAppExceptionMessage(e);
-            return getGameStatusOption(gameApplication);
         }
     }
 }
