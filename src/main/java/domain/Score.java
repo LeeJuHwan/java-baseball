44f0badf9d7c22ef1381aed21cb234947a87eabc
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-08-23 12:52:09 +0900

    refactor: 스코어 객체의 매직 스트링 상수 추출

--- src/main/java/domain/Score.java
+++ src/main/java/domain/Score.java
@@ -9,6 +9,9 @@ public class Score {
     private int strikeCount;
     private int ballCount;
 
+    private static final String BALL_MESSAGE = "볼";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String UN_HANDLE_MESSAGE = "낫싱";
 
     void clean() {
         this.strikeCount = 0;
@@ -27,12 +30,17 @@ public class Score {
         if (ballCount > 0 && strikeCount > 0) {
             OutputHandler.printMessage(ballCount + "볼 " + strikeCount + "스트라이크");
             return;
+            return ballCount + BALL_MESSAGE +  " " + strikeCount + STRIKE_MESSAGE;
+            return ballCount + BALL_MESSAGE;
+            return strikeCount + STRIKE_MESSAGE;
         }
 
-        if (ballCount > 0) {
-            OutputHandler.printMessage(ballCount + "볼");
-            return;
-        }
+        return UN_HANDLE_MESSAGE;
+    }
+
+    private boolean isOnlyStrike() {
+        return strikeCount > 0;
+    }
 
         if (strikeCount > 0) {
             OutputHandler.printMessage(strikeCount + "스트라이크");
