6c0ed041b68609093fa2e610e4297afa31da4b28
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-08-24 01:59:43 +0900

    update: 어플리케이션 인터페이스 메서드 축소

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -2,12 +2,5 @@ package base;
 
 public interface Application {
 
-    boolean isGameRestart(String gameFlag);
-
-    boolean isGameStop(String gameFlag);
-
-    String selectGameRestartOrStop();
-
-    void run();
-
+    void startGame();
 }
