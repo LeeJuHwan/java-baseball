dc6a8ad142459f9386ddfd559c3e64ea353c8eca
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-10-23 23:18:10 +0900

    refactor: 스코어 객체에서 카운트 계산 식 삭제 및 카운트 일급 컬렉션 추가

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,47 +1,42 @@
 package baseball.domain;
 
 import baseball.domain.count.Count;
-import java.util.List;
+import baseball.domain.count.Counts;
 
 public class Score {
 
-    private final long strikeCount;
-    private final long ballCount;
+    private final Counts counts;
 
-    private Score(long strikeCount, long ballCount) {
-        this.strikeCount = strikeCount;
-        this.ballCount = ballCount;
+    private Score(Counts counts) {
+        this.counts = counts;
     }
 
-    public static Score from(List<Count> counts) {
-        long ballCount = counts.stream().filter(count -> count == Count.BALL).count();
-        long strikeCount = counts.stream().filter(count -> count == Count.STRIKE).count();
-
-        return new Score(strikeCount, ballCount);
+    public static Score from(Counts counts) {
+        return new Score(counts);
     }
 
     public String getScreenMessage() {
         if (isStrikeCountGreaterThanZero() && isBallCountGreaterThanZero()) {
-            return strikeCount + Count.STRIKE.description + " " + ballCount + Count.BALL.description;
+            return counts.getStrikeCount() + Count.STRIKE.description + " " + counts.getBallCount() + Count.BALL.description;
         }
 
         if (isStrikeCountGreaterThanZero()) {
-            return strikeCount + Count.STRIKE.description;
+            return counts.getStrikeCount() + Count.STRIKE.description;
         }
 
         if (isBallCountGreaterThanZero()) {
-            return ballCount + Count.BALL.description;
+            return counts.getBallCount() + Count.BALL.description;
         }
 
         return Count.NOTHING.description;
     }
 
     private boolean isBallCountGreaterThanZero() {
-        return ballCount > 0;
+        return counts.getBallCount() > 0;
     }
 
     private boolean isStrikeCountGreaterThanZero() {
-        return strikeCount > 0;
+        return counts.getStrikeCount() > 0;
     }
 
 }
--- src/test/java/baseball/domain/ScoreTest.java
+++ src/test/java/baseball/domain/ScoreTest.java
@@ -1,10 +1,12 @@
 package baseball.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import baseball.domain.count.Count;
+import baseball.domain.count.Counts;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,11 +14,30 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class ScoreTest {
 
+    private List<Count> setCounts;
+
+    @BeforeEach
+    void setUp() {
+        setCounts = new ArrayList<>();
+        setCounts.add(Count.NOTHING);
+        setCounts.add(Count.NOTHING);
+        setCounts.add(Count.NOTHING);
+    }
+
+    @Test
+    @DisplayName("Counts 객체는 매번 새로운 인스턴스가 생성되어야 한다")
+    void test_counts_객체_생성_검증() {
+        Counts counts1 = Counts.of(Count.NOTHING, Count.NOTHING, Count.NOTHING);
+        Counts counts2 = Counts.of(Count.NOTHING, Count.NOTHING, Count.NOTHING);
+
+        assertThat(counts1).isNotSameAs(counts2);
+    }
+
     @Test
     @DisplayName("카운트 리스트 스코어 낫띵")
     void test_스코어_낫띵() {
         Score score = Score.from(
-                List.of(
+                Counts.of(
                         Count.NOTHING,
                         Count.NOTHING,
                         Count.NOTHING
@@ -30,17 +51,17 @@ public class ScoreTest {
     @DisplayName("카운트 리스트 스코어 스트라이크 카운트 검증")
     @CsvSource(value = {"1,1스트라이크", "2,2스트라이크", "3,3스트라이크"})
     void test_스코어_스트라이크_메세지(int strikeCount, String scoreMessage) {
-        List<Count> ballList = new ArrayList<>();
-
         for (int i = 0; i < strikeCount; i++) {
-            ballList.add(Count.STRIKE);
+            setCounts.set(i, Count.STRIKE);
         }
 
-        while (ballList.size() < 3) {
-            ballList.add(Count.NOTHING);
-        }
-
-        Score score = Score.from(ballList);
+        Score score = Score.from(
+                Counts.of(
+                        setCounts.get(0),
+                        setCounts.get(1),
+                        setCounts.get(2)
+                )
+        );
 
         assertThat(score.getScreenMessage()).isEqualTo(scoreMessage);
     }
@@ -50,17 +71,18 @@ public class ScoreTest {
     @DisplayName("카운트 리스트 스코어 볼 카운트 검증")
     @CsvSource(value = {"1,1볼", "2,2볼", "3,3볼"})
     void test_스코어_볼_메세지(int ballCount, String scoreMessage) {
-        List<Count> ballList = new ArrayList<>();
 
         for (int i = 0; i < ballCount; i++) {
-            ballList.add(Count.BALL);
+            setCounts.set(i, Count.BALL);
         }
 
-        while (ballList.size() < 3) {
-            ballList.add(Count.NOTHING);
-        }
-
-        Score score = Score.from(ballList);
+        Score score = Score.from(
+                Counts.of(
+                        setCounts.get(0),
+                        setCounts.get(1),
+                        setCounts.get(2)
+                )
+        );
 
         assertThat(score.getScreenMessage()).isEqualTo(scoreMessage);
     }
@@ -69,7 +91,7 @@ public class ScoreTest {
     @DisplayName("카운트 리스트 스코어 1볼 1스트라이크")
     void test_스코어_1볼_1스트라이크() {
         Score score = Score.from(
-                List.of(
+                Counts.of(
                         Count.BALL,
                         Count.STRIKE,
                         Count.NOTHING
